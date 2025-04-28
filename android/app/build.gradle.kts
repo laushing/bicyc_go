194de4d1687b28c51e3d0019e486328bdd2ede60
chi shing lau <xchi.lau@gmail.com>
2025-04-28 13:15:26 +0800

    更新ndk問題

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.bicyc_go"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
